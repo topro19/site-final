006d2effcf20377a1ea6deeb05723a6cf2118241
topro19 <140230546+topro19@users.noreply.github.com>
2026-05-14 15:12:13 +0530

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,16 @@
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        about: resolve(__dirname, 'about.html'),
+        contact: resolve(__dirname, 'contact.html'),
+        exterior: resolve(__dirname, 'exterior.html'),
+        interior: resolve(__dirname, 'interior.html'),
+      },
+    },
+  },
+})
